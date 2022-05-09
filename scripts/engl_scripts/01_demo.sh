88618d60e3d5c8b3b2194f8590aa482074ceec98
xiaoxiaomiao39 <miao@gpuhost06.cm.cluster>
2022-05-09 12:26:18 +0900

    update

--- scripts/engl_scripts/01_demo.sh
+++ scripts/engl_scripts/01_demo.sh
@@ -9,15 +9,16 @@ source env.sh
 
 
 #download pretrain models
-if [ ! -e "pretrained_models/" ]; then
-    if [ -f pretrained_models.tar.gz ];
+if [ ! -e "pretrained_models_anon_xv/" ]; then
+    if [ -f pretrained_models_anon_xv.tar.gz ];
     then
-        rm pretrained_models.tar.gz
+        rm pretrained_models_anon_xv.tar.gz
     fi
     echo -e "${RED}Downloading pre-trained model${NC}"
 
     #wget https://zenodo.org/record/6369772/files/pretrained_models.tar.gz
-    #cd /home/smg/miao/zenono/pretrained_models_sas_2022/pretrained_models_anon_xv/HuBERT_soft
+    tar -xzvf pretrained_models_anon_xv.tar.gz
+    cd pretrained_models_anon_xv/
     wget https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt
     cd $home
 fi
@@ -25,7 +26,7 @@ fi
 
 
 # try pre-trained model
-if [ -e "data/aishell3/" ];then
+if [ -e "data/libri_dev/" ];then
    echo -e "${RED}Try pre-trained model${NC}"
    #for model_type in {multilan_fbank_xv_ssl_freeze,libri_tts_clean_100_fbank_xv_ssl_freeze}; do
    model_type=libri_tts_clean_100_fbank_xv_ssl_freeze   
@@ -34,9 +35,9 @@ if [ -e "data/aishell3/" ];then
 		libri_test_{enrolls,trials_f,trials_m} \
 		vctk_test_{enrolls,trials_f_all,trials_m_all}; do
         python adapted_from_facebookresearch/inference.py --input_test_file scp/vpc/$dset.lst \
-		    --xv_dir data/vpc/$dset/pseudo_xvectors/xvectors \
-		    --checkpoint_file pretrained_models/pretrain_hifigan/$model_type \
-		    --output_dir pretrained_models/output/$model_type/${dset}
+		    --xv_dir pretrained_models_anon_xv/anon_spk_vector/$dset/pseudo_xvectors/xvectors \
+		    --checkpoint_file pretrained_models_anon_xv/HiFi-GAN/$model_type \
+		    --output_dir pretrained_models_anon_xv/output/$model_type/${dset}
    done
    echo -e "${RED}Please check generated waveforms from pre-trained model in ./pretrained_models/output"
     
