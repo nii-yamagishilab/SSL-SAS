d28b197bdc9f10a3b93a6f1c45f6456dd6d88754
xiaoxiaomiao39 <miao@gpuhost06.cm.cluster>
2023-09-27 09:49:57 +0900

    add selection based anonymizer

--- env.sh
+++
@@ -1,2 +0,0 @@
-if [ $(which python) != /home/smg/miao/github_code/ssl_sas/ssl-sas-select/venv/bin/python ]; then source /home/smg/miao/github_code/ssl_sas/ssl-sas-select/venv/bin/activate; fi
-export PATH=/home/smg/miao/github_code/ssl_sas/ssl-sas-select/STL-2009/src/sv56:$PATH
